770c1e89157e947b533972a887f39a5555dd305c
Greg Meyer <gregory.meyer@gmail.com>
2018-03-22 15:42:30 -0700

    add some checks for validation of command line arguments

---
+++ validate.py
@@ -0,0 +1,28 @@
+'''
+This file is part of BUGOUT
+(c) Greg Meyer, 2018
+'''
+
+from os.path import isdir, isfile
+
+def directories(dirs):
+    rtn = []
+    for d in dirs:
+        if not isdir(d):
+            raise ValueError('command line argument "%s" is not a directory.' % d)
+        else:
+            rtn.append(d)
+    return rtn
+
+def outfile(f):
+    if isfile(f):
+        print('Output file exists. Overwrite? (y/n): ',end='')
+        if input() != 'y':
+            print('Exiting...')
+            exit()
+
+    elif isdir(f):
+        raise ValueError('Output file is a directory! Did you forget to pass '
+                         'a filename to the -m argument?')
+
+    return f
